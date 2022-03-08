a684379ad97dd3657d1b22e8f58906b0093b21c7
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-03-08 17:27:04 -0500

    name loop can now handle null/blank values and require a valid entry

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,7 +80,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-
+      
       // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
 
@@ -167,11 +167,24 @@ var shop = function() {
 
 /* END GAME FUNCTIONS */
 
-/* GAME INFORMATION / VARIABLES */
 
+
+/* GAME INFORMATION / VARIABLES */
 // player information
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+while (name === "" || name === null) {
+  name = prompt("What is your robot's name?");
+}
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
